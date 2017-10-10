5b0638fc6387aca58f9bd4b2cfefb8a0825579e0
Sebastião <sebasti3d@hotmail.com>
2017-10-10 16:22:48 +0100

    10 oct

--- scripts/basicJS.js
+++ scripts/basicJS.js
@@ -5,3 +5,9 @@ function submited() {
 function pageWIP() {
     alert("Page in Progress.");
 }
+
+function getYear() {
+    var today = new Date()
+    var year = today.getFullYear()
+    document.write(year)
+}
